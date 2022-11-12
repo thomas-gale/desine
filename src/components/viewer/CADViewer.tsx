b620874df4e85a53dccfc007836166b33f11c72a
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-12 11:44:22 +0000

    Loading basic example char.step from IPFS and rendering in three.js canvas.

--- src/components/viewer/CADViewer.tsx
+++ src/components/viewer/CADViewer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import occtimportjs from "occt-import-js";
 
 function Box(props: ThreeElements["mesh"]) {
@@ -27,25 +28,69 @@ export interface CADViewerProps {
   stepURL: string;
 }
 
-export const CADViewer = ({ stepURL: url }: CADViewerProps) => {
+export const CADViewer = ({ stepURL }: CADViewerProps) => {
   const object = useRef<THREE.Object3D>(null!);
 
   useEffect(() => {
     (async () => {
-      console.log("CADViewer: about to load URL: ", url);
-      console.log("occt: ", occtimportjs);
+      console.log("CADViewer: about to load URL: ", stepURL);
       const occt = await occtimportjs();
-      console.log("CADViewer: occt: ", occt);
+
+      // download step file
+      let response = await fetch(stepURL);
+      let buffer = await response.arrayBuffer();
+
+      // read the imported step file
+      let fileBuffer = new Uint8Array(buffer);
+      let result = occt.ReadStepFile(fileBuffer, null);
+
+      // process the geometries of the result
+      for (let resultMesh of result.meshes) {
+        let geometry = new THREE.BufferGeometry();
+
+        geometry.setAttribute(
+          "position",
+          new THREE.Float32BufferAttribute(
+            resultMesh.attributes.position.array,
+            3
+          )
+        );
+        if (resultMesh.attributes.normal) {
+          geometry.setAttribute(
+            "normal",
+            new THREE.Float32BufferAttribute(
+              resultMesh.attributes.normal.array,
+              3
+            )
+          );
+        }
+        const index = Uint32Array.from(resultMesh.index.array);
+        geometry.setIndex(new THREE.BufferAttribute(index, 1));
+
+        let material = null;
+        if (resultMesh.color) {
+          const color = new THREE.Color(
+            resultMesh.color[0],
+            resultMesh.color[1],
+            resultMesh.color[2]
+          );
+          material = new THREE.MeshPhongMaterial({ color: color });
+        } else {
+          material = new THREE.MeshPhongMaterial({ color: 0xcccccc });
+        }
+
+        const mesh = new THREE.Mesh(geometry, material);
+        object.current.add(mesh);
+      }
     })();
-  }, [url]);
+  }, [stepURL]);
 
   return (
     <Canvas>
+      <OrbitControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <object3D ref={object} />
+      <object3D ref={object} position={[0, 0, 0]} scale={[0.01, 0.01, 0.01]} />
     </Canvas>
   );
 };
