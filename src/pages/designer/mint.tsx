eb20136eb5f12125e6191424db79d9ac7cabd883
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-19 14:35:22 +0000

    Starting on stepper for minting.

--- src/pages/designer/mint.tsx
+++ src/pages/designer/mint.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { Button } from "../../components/elements/Button";
 import { Spinner } from "../../components/elements/Spinner";
 import { CADViewer } from "../../components/viewer/CADViewer";
 import { useWrapIpfsGateway } from "../../hooks/useWrapIpfsGateway";
@@ -9,6 +10,16 @@ const Mint = (): JSX.Element => {
   const { cid } = router.query;
 
   const [step, setStep] = useState<"model" | "metadata" | "mint">("model");
+  const stepIndex = useMemo(() => {
+    switch (step) {
+      case "model":
+        return 0;
+      case "metadata":
+        return 1;
+      case "mint":
+        return 2;
+    }
+  }, [step]);
 
   if (!cid) {
     return <div />;
@@ -23,13 +34,17 @@ const Mint = (): JSX.Element => {
           Review Model
         </li>
         <li
-          className="step step-primary hover:text-primary cursor-pointer"
+          className={`step ${
+            stepIndex >= 1 && "step-primary"
+          } hover:text-primary cursor-pointer`}
           onClick={() => setStep("metadata")}
         >
           Define Metadata
         </li>
         <li
-          className="step step-primary hover:text-primary cursor-pointer"
+          className={`step ${
+            stepIndex >= 2 && "step-primary"
+          } hover:text-primary cursor-pointer`}
           onClick={() => setStep("mint")}
         >
           Mint ERC1155 NFT
@@ -42,6 +57,59 @@ const Mint = (): JSX.Element => {
               Model: <b>{cid}</b>
             </h2>
             <CADViewer stepURL={cid as string} />
+            <Button
+              className="no-animation"
+              onClick={() => setStep("metadata")}
+              external={false}
+            >
+              Next
+            </Button>
+          </>
+        )}
+        {step === "metadata" && (
+          <>
+            <h2>Define Metadata</h2>
+            <div className="h-full">TODO</div>
+            <div className="flex flex-row space-x-2">
+              <Button
+                className="no-animation flex-grow"
+                onClick={() => setStep("model")}
+                external={false}
+              >
+                Previous
+              </Button>
+              <Button
+                className="no-animation flex-grow"
+                onClick={() => setStep("mint")}
+                external={false}
+              >
+                Next
+              </Button>
+            </div>
+          </>
+        )}
+        {step === "mint" && (
+          <>
+            <h2>Review and confirm Minting</h2>
+            <div className="h-full">TODO</div>
+            <div className="flex flex-row space-x-2">
+              <Button
+                className="no-animation flex-grow"
+                onClick={() => setStep("metadata")}
+                external={false}
+              >
+                Previous
+              </Button>
+              <Button
+                className="no-animation flex-grow"
+                onClick={() =>
+                  console.log("TODO: Trigger web3 wallet operation")
+                }
+                external={false}
+              >
+                Mint
+              </Button>
+            </div>
           </>
         )}
       </div>
