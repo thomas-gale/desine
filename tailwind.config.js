91c9d8c72cc2d6da6fa24a46206540bcc8503ea6
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-20 13:03:46 +0000

    locking to dark theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+    themes: ["dark"],
   },
 };
