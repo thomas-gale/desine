95b8c537510224372f60aa7ea41b3412441abdd8
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-21 11:11:35 +0000

    Making progress on the DesineToken.sol.

--- contracts/DesineToken.sol
+++ contracts/DesineToken.sol
@@ -2,35 +2,60 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "hardhat/console.sol";
 
-contract DesineToken is ERC1155, Ownable {
-    // TODO Add token id array and count (for enumeration)
-    mapping(uint256 => string) private _cids;
-    mapping(uint256 => string) private _metadata_cids;
+contract DesineToken is ERC1155 {
+    uint256[] public tokenIds;
+    mapping(uint256 => string) public cids;
+    mapping(uint256 => string) public metadataCids;
 
     constructor() ERC1155("") {
         console.log("Constructing DesineToken contract");
     }
 
-    function mint(string memory cid, string memory metadata_cid)
-        public
-        onlyOwner
-    {
+    function getNumberTokenIds() public view returns (uint256) {
+        return tokenIds.length;
+    }
+
+    function getTokenIds() public view returns (uint256[] memory) {
+        return tokenIds;
+    }
+
+    // TODO check if rather than hash, which has probabilistic clash, we could convert cid (base58) to uint256 directly
+    function computeTokenId(string memory cid) public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(cid)));
+    }
+
+    function mint(string memory cid, string memory metadataCid) public {
         console.log(
-            "Attempting to minting DesineToken for owner %s: cid: %s, metadata_cid",
+            "DesineToken.mint: minter %s: cid: %s, metadata_cid:",
             msg.sender,
             cid,
-            metadata_cid
+            metadataCid
         );
+
+        uint256 tokenId = computeTokenId(cid);
+        console.log("Computed uint256 token id:", tokenId);
+
         // Check if the token already exists and revert if so.
-        uint256 tokenId = uint256(keccak256(abi.encodePacked(cid)));
-        console.log("Computed token id", tokenId);
-        _cids[tokenId] = cid;
-        _metadata_cids[tokenId] = metadata_cid;
-        // TODO Add token id to array and increment count (for enumeration)
+        require(
+            bytes(cids[tokenId]).length == 0,
+            "DesineToken.mint: token already exists"
+        );
+
+        tokenIds.push(tokenId);
+        cids[tokenId] = cid;
+        metadataCids[tokenId] = metadataCid;
         _mint(msg.sender, tokenId, 1, "");
+    }
+
+    function balanceOfCid(address account, string memory cid)
+        public
+        view
+        returns (uint256)
+    {
+        console.log("DesineToken.balanceOf: account %s, id %s", account, cid);
+        return super.balanceOf(account, computeTokenId(cid));
     }
 }
--- test/DesineToken.test.js
+++ test/DesineToken.test.js
@@ -8,13 +8,38 @@ describe("DesineToken contract", function () {
     await deployedContract.deployed();
     return { owner, addr1, addr2, deployedContract };
   }
+  async function deployContractFixtureWithSomeTokens() {
+    const { owner, addr1, addr2, deployedContract, ...rest } =
+      await deployContractFixture();
+    const cid1 = "1234";
+    const cid2 = "12345";
+    const cid3 = "123456";
+    await deployedContract.connect(owner).mint(cid1, cid1);
+    await deployedContract.connect(addr1).mint(cid2, cid2);
+    await deployedContract.connect(addr2).mint(cid3, cid3);
+    return { owner, addr1, addr2, deployedContract, ...rest };
+  }
   it("Should deploy", async function () {
     const { deployedContract } = await deployContractFixture();
     expect(deployedContract).to.not.equal(undefined);
   });
-  it("Should owner mint", async function () {
-    const { owner, deployedContract } = await deployContractFixture();
-    const mintRes = await deployedContract.mint("asdf", "asdf");
-    expect(mintRes).to.not.equal(undefined);
+  it("Should addr1 mint", async function () {
+    const { addr1, deployedContract } = await deployContractFixture();
+    const cid = "12345";
+    const metadata_cid = "metadata";
+    await deployedContract.connect(addr1).mint(cid, metadata_cid);
+    const balance = await deployedContract.balanceOfCid(addr1.address, cid);
+    expect(balance).to.equal(1);
+  });
+  it("Should list all tokens", async function () {
+    const { deployedContract } = await deployContractFixtureWithSomeTokens();
+    const numberTokenIds = await deployedContract.getNumberTokenIds();
+    expect(numberTokenIds).to.equal(3);
+    const tokenIds = await deployedContract.getTokenIds();
+    for (const tokenId of tokenIds) {
+      const tokenCid = await deployedContract.cids(tokenId);
+      console.log("token id: %s, token cid:", tokenId, tokenCid);
+      expect(tokenCid).to.not.equal(undefined);
+    }
   });
 });
